38c8fbea2dd22b32a5fa3793f7fa34133c5b2e07
m0viefreak <m0viefreak.cm@googlemail.com>
2016-03-28 14:55:56 +0200

    root-highlighter: use EUID instead of id -u command
    No need to fork every time.

--- highlighters/root/root-highlighter.zsh
+++ highlighters/root/root-highlighter.zsh
@@ -40,5 +40,5 @@ _zsh_highlight_root_highlighter_predicate()
 # root highlighting function.
 _zsh_highlight_root_highlighter()
 {
-  if [[ $(command id -u) -eq 0 ]] { region_highlight+=("0 $#BUFFER $ZSH_HIGHLIGHT_STYLES[root]") }
+  if (( EUID == 0 )) { region_highlight+=("0 $#BUFFER $ZSH_HIGHLIGHT_STYLES[root]") }
 }
