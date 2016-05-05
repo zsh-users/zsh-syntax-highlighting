zsh-users/zsh-syntax-highlighting

commit 95d82568d8f9d4ba34eb293b6ce792854a14f110

'brackets': Use one-based indexing
zsh arrays start at one, and this will make the next commit easier.

highlighters/brackets/brackets-highlighter.zsh
@@ -51,8 +51,8 @@ _zsh_highlight_brackets_highlighter()
   local -A levelpos lastoflevel matching typepos
 
   # Find all brackets and remember which one is matching
-  for (( pos = 0; pos < buflen; pos++ )) ; do
-    char="$BUFFER[pos+1]"
+  for (( pos = 1; pos <= buflen; pos++ )) ; do
+    char=$BUFFER[pos]
     case $char in
       ["([{"])
         levelpos[$pos]=$((++level))
@@ -83,7 +83,7 @@ _zsh_highlight_brackets_highlighter()
     else
       style=bracket-error
     fi
-    _zsh_highlight_add_highlight $pos $((pos + 1)) $style
+    _zsh_highlight_add_highlight $((pos - 1)) $pos $style
   done
 
   # If cursor is on a bracket, then highlight corresponding bracket, if any

commit ccaf6f48a10e7653c2e4b80571e59d07c0de442f

brackets: Fix bug introduced in 95d8256

highlighters/brackets/brackets-highlighter.zsh
@@ -46,7 +46,7 @@ _zsh_highlight_brackets_highlighter_predicate()
 # Brackets highlighting function.
 _zsh_highlight_brackets_highlighter()
 {
-  local char quotetype style
+  local char style
   local -i bracket_color_size=${#ZSH_HIGHLIGHT_STYLES[(I)bracket-level-*]} buflen=${#BUFFER} level=0 matchingpos pos
   local -A levelpos lastoflevel matching
 
@@ -68,11 +68,7 @@ _zsh_highlight_brackets_highlighter()
         ;;
       ['"'\'])
         # Skip everything inside quotes
-        quotetype=$char
-        while (( pos < buflen )) ; do
-          (( pos++ ))
-          [[ $BUFFER[pos+1] == $quotetype ]] && break
-        done
+	pos=$BUFFER[(ib:pos+1:)$char]
         ;;
     esac
   done