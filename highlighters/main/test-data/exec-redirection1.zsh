fa57633d81526451248919210ae4aad731a76a0e
Daniel Shahaf <d.s@daniel.shahaf.name>
2016-07-28 08:13:51 +0000

    'main': Enable test for issue #238.

--- highlighters/main/test-data/exec-redirection1.zsh
+++ highlighters/main/test-data/exec-redirection1.zsh
@@ -31,7 +31,7 @@ BUFFER='exec {foo}>&/tmp ls'
 
 expected_region_highlight=(
   "1 4 precommand" # exec
-  # TODO: "6 10 redirection 'issue #238'" # {foo}
+  "6 10 redirection 'issue #238'" # {foo}
   "11 12 redirection" # >&
   "13 16 path" # /tmp
   "18 19 command 'issue #238'" # ls
