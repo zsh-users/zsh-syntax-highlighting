1fee620e6235d3d1789dd3b62e65f5618464dbea
Daniel Shahaf <danielsh@apache.org>
2018-01-12 13:02:39 +0000

    'main': Remove superfluous variable. No functional change.

--- highlighters/main/main-highlighter.zsh
+++ highlighters/main/main-highlighter.zsh
@@ -456,8 +456,7 @@ _zsh_highlight_highlighter_main_paint()
     () {
       # TODO: path expansion should happen _after_ alias expansion
       _zsh_highlight_main_highlighter_expand_path $arg
-      local expanded_arg="$REPLY"
-      _zsh_highlight_main__type ${expanded_arg}
+      _zsh_highlight_main__type "$REPLY"
     }
     local res="$REPLY"
     if [[ $res == "alias" ]]; then
