42d396d8190b93ae0bb45ed22039589b4840ab82
Adam Johnson <me@adamj.eu>
2018-09-07 18:21:53 +0000

    driver: Exclude yank-pop again (#183).

--- zsh-syntax-highlighting.zsh
+++ zsh-syntax-highlighting.zsh
@@ -305,7 +305,7 @@ _zsh_highlight_bind_widgets()
 
   # Override ZLE widgets to make them invoke _zsh_highlight.
   local -U widgets_to_bind
-  widgets_to_bind=(${${(k)widgets}:#(.*|run-help|which-command|beep|set-local-history|yank)})
+  widgets_to_bind=(${${(k)widgets}:#(.*|run-help|which-command|beep|set-local-history|yank|yank-pop)})
 
   # Always wrap special zle-line-finish widget. This is needed to decide if the
   # current line ends and special highlighting logic needs to be applied.
