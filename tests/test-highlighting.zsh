9bdeb4aa4aa17cd5ad09557b5c1e3d3c75ebe6f0
Daniel Shahaf <d.s@daniel.shahaf.name>
2020-03-19 00:03:24 +0000

    test harness: Remove a bogus check.
    We already declare $expected_region_highlight in run_test_internal().
    Therefore, it will always be declared.

--- tests/test-highlighting.zsh
+++ tests/test-highlighting.zsh
@@ -135,8 +135,6 @@ run_test_internal() {
 
     # Check the data declares $PREBUFFER or $BUFFER.
     [[ -z $PREBUFFER && -z $BUFFER ]] && { echo >&2 "Bail out! On ${(qq)1}: Either 'PREBUFFER' or 'BUFFER' must be declared and non-blank"; return ${RETURN:=1}; }
-    # Check the data declares $expected_region_highlight.
-    (( $+expected_region_highlight == 0 )) && { echo >&2 "Bail out! On ${(qq)1}: 'expected_region_highlight' is not declared."; return ${RETURN:=1}; }
 
     # Set sane defaults for ZLE variables
     : ${CURSOR=$#BUFFER} ${PENDING=0} ${WIDGET=z-sy-h-test-harness-test-widget}
