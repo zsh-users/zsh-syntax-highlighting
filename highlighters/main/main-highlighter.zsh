0e1bb14452e3fc66dcc81531212e1061e02c1a61
Matthew Martin <phy1729@gmail.com>
2021-05-29 23:24:20 -0500

    main: precommands += proxychains

--- highlighters/main/main-highlighter.zsh
+++ highlighters/main/main-highlighter.zsh
@@ -362,6 +362,7 @@ _zsh_highlight_highlighter_main_paint()
     'env' u:i
     'ionice' cn:t:pPu # util-linux 2.33.1-0.1
     'strace' IbeaosXPpEuOS:ACdfhikqrtTvVxyDc # strace 4.26-0.2
+    'proxychains' q:f # proxychains 4.4.0
 
     # As of OpenSSH 8.1p1
     'ssh-agent' aEPt:csDd:k
